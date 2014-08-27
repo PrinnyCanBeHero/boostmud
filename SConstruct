02edc86b195ef1ffb63b10c329fc5275b9a9cbd1
Christopher Best <mrmerrow@monkeyinfinity.net>
2014-08-26 22:02:42 -0700

    Condition variables not working.
    Got a simple echo server working.

---
+++ SConstruct
@@ -0,0 +1,10 @@
+import os
+
+env = Environment()
+env.Append(CCFLAGS='-std=c++11')
+env.Append(LIBS=['boost_thread-mt', 'boost_system-mt'])
+
+boostmud = env.Program(target='boostmud',
+  source = Glob('*.cpp'))
+
+Default(boostmud)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include "world.hpp"
+#include "tcpserver.hpp"
+
+#include <signal.h>
+
+#include <iostream>
 
 using namespace boostMUD::world;
+using namespace boostMUD::util;
+
+boost::mutex sleepy_mutex;
+boost::condition_variable sleepy_variable;
+
+bool done = false;
 
 class TestRoom : public Room 
 {
@@ -23,6 +34,25 @@ private:
 
 };
 
+class EchoSession : public TcpSession 
+{
+public:
+	EchoSession(boost::asio::io_service &io)
+	: TcpSession(io) { }
+	virtual size_t preferredChunkSize() const { return 128u;  }
+
+	virtual void handleIncoming(const ByteArray& bytes) {
+		do_write(std::make_shared<ByteArray>(bytes));
+	}
+};
+
+TcpSession::pointer make_echo(boost::asio::io_service& io)
+{
+	return std::make_shared<EchoSession>(io);
+}
+
+void SigHandler(int signal);
+
 int main(int argc, char* argv[]) {
 
 	World world;
@@ -33,6 +63,36 @@ int main(int argc, char* argv[]) {
 	world.add(std::make_shared<TestRoom>(id++, "Test room 2"));
 	world.add(std::make_shared<TestRoom>(id++, "Test room 3"));
 	
+	::signal(SIGINT, SigHandler);
+	::signal(SIGTERM, SigHandler);
+	
+	TcpServer server(4004, make_echo);
+	
+	std::cout << "Sleeping..." << std::endl;
+	
+	//boost::mutex::scoped_lock lock(sleepy_mutex);
+	//sleepy_variable.wait(lock);
+	
+	//std::cout << "Main thread woke up!" << std::endl;
+	
+	while(!done) {
+		boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+	}
+	
+	std::cout << "Exiting..." << std::endl;
+	
 
 return 0;
+}
+
+void SigHandler(int signal) {
+	if (signal == SIGINT || signal == SIGTERM) {
+		std::cout << "Received shutdown sig!" << std::endl;
+		//::signal(signal,SIG_IGN);
+		//boost::mutex::scoped_lock lock(sleepy_mutex);
+		//std::cout << "Sleepy mutex locked!" << std::endl;
+		//sleepy_variable.notify_all();
+		//std::cout << "Woke up main thread." << std::endl;
+		done = true;
+	}
 }
